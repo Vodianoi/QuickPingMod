21d1afe419a8eaf6531a0844bc11f52e588b9c13
Vodyanoi <axepoty@gmail.com>
2022-11-25 03:42:05 +0100

    Move Recursive methods to Utilities as they extend GameObject

---
+++ QuickPing/Utilities/Utils.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace QuickPing.Utilities
+{
+    internal static class Utils
+    {
+        public static IDestructible GetRecursiveComponentInParents<IDestructible>(Transform root)
+
+        {
+            if (root.parent == null) return default;
+
+            var res = root.GetComponentInParent<IDestructible>();
+            if (res != null)
+                return res;
+            else
+                return GetRecursiveComponentInParents<IDestructible>(root.parent);
+        }
+
+
+        public static GameObject GetRecursiveParentWithComponent<T>(Transform root)
+        {
+            if (!root.parent) return null;
+            if (root.parent.gameObject.TryGetComponent(out T _))
+            {
+                return root.parent.gameObject;
+            }
+            else
+                return GetRecursiveParentWithComponent<T>(root.parent);
+        }
+    }
+}
