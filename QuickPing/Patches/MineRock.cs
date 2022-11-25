0b44b18369dad48e33cdc6174f8dc671606ee5a7
Vodyanoi <axepoty@gmail.com>
2022-11-25 03:48:01 +0100

    Add automatic pin removing on destroy. #8 Works until you disconnect ; after, pins won't remove themselves

---
+++ QuickPing/Patches/MineRock.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+
+namespace QuickPing.Patches
+{
+    [HarmonyPatch(typeof(MineRock5))]
+    internal static class MineRock5_Patch
+    {
+        [HarmonyPatch(typeof(MineRock5), nameof(MineRock5.AllDestroyed))]
+        [HarmonyPostfix]
+        public static void AllDestroyed(MineRock5 __instance, bool __result)
+        {
+            if (!__result) return;
+
+            var pos = __instance.transform.position;
+            if (Minimap_Patch.RemovePin(pos, Minimap.PinType.Icon2))
+            {
+                QuickPing.Log.LogWarning($"Removed ping at x:{pos.x}, y:{pos.y}, z:{pos.z}");
+            }
+        }
+    }
+}
