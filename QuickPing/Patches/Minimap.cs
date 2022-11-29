1e52e33a370bd4896e8f1463d7d4244521e8f419
Vodianoi <axepoty@gmail.com>
2022-11-29 18:38:48 +0100

    Add save/load system for persistent data, works with steam cloud and local worlds

--- QuickPing/Patches/Minimap.cs
+++ QuickPing/Patches/Minimap.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
-using static Minimap;
 
 namespace QuickPing.Patches
 {
@@ -9,12 +10,15 @@ namespace QuickPing.Patches
     /// </summary>
     internal static class Minimap_Patch
     {
+        public static Dictionary<ZDOID, Minimap.PinData> PinnedObjects = new();
+
+
         /// <summary>
         /// 
         /// </summary>
         /// <param name="strID"></param>
         /// <returns></returns>
-        public static PinType IsPinable(string strID)
+        public static Minimap.PinType IsPinable(string strID)
         {
 
             string baseLocalization = Localization_Patch.GetBaseTranslation(strID);
@@ -91,23 +95,32 @@ namespace QuickPing.Patches
                     return pinType;
                 }
             }
-            return PinType.None;
+            return Minimap.PinType.None;
         }
 
         /// <summary>
         /// Add correct pin to map if Settings.
         /// </summary>
         /// <param name="obj"></param>
-        public static void AddPin(HoverObject obj) => AddPin(obj.hover, obj.destructible, obj.name, obj.center);
+        public static void AddPin(HoverObject obj) => AddPin(obj.Hover, obj.Destructible, obj.Name, obj.center);
+        /// <summary>
+        /// Add correct pin to map if Settings and bypass filters.
+        /// </summary>
+        /// <param name="obj"></param>
+        public static void ForceAddPin(HoverObject obj) => AddPin(obj.Hover, obj.Destructible, obj.Name, obj.center, true);
+
         /// <summary>
         /// Add correct pin to map if Settings.
         /// </summary>
-        /// <param name="strID"></param>
-        /// <param name="pos"></param>
-        public static void AddPin(GameObject hover, IDestructible idestructible, string strID, Vector3 pos)
+        /// <param name="hover">Pinged object</param>
+        /// <param name="idestructible">IDestructible interface found in "hover"</param>
+        /// <param name="strID">Display Name of hover object</param>
+        /// <param name="pos">Pinged position (last raycast point or hover center)</param>
+        /// <param name="force">Bypass filters</param>
+        public static void AddPin(GameObject hover, IDestructible idestructible, string strID, Vector3 pos, bool force = false)
         {
 
-            if (string.IsNullOrEmpty(strID) || !Settings.AddPin.Value || hover == null) { return; }
+            if ((string.IsNullOrEmpty(strID) || !Settings.AddPin.Value || hover == null) && !force) { return; }
 
             Minimap.PinData pinData = new Minimap.PinData
             {
@@ -115,65 +128,68 @@ namespace QuickPing.Patches
             };
 
 
-            if (pinData.m_type != PinType.None)
+            Minimap.PinData closestPin = Minimap.instance.GetClosestPin(pos, Settings.ClosestPinRange.Value);
+            if (pinData.m_type != Minimap.PinType.None)
             {
 
                 pinData.m_name ??= Localization.instance.Localize(strID);
-
+                pinData.m_pos = pos;
                 //Check if there is another pin in range
-                Minimap.PinData closestPin = Minimap.instance.GetClosestPin(pos, Settings.ClosestPinRange.Value);
 
                 //PORTAL :Check if an already existing pin is at pos
                 if (strID == "$piece_portal")
                 {
                     if (hover.TryGetComponent(out Hoverable hoverable))
                     {
-                        string tag = GetPortalTag(hoverable);
-                        Jotunn.Logger.LogInfo(tag);
-                        pinData.m_name = tag;
-                        //TODO CHECK BEFORE REMOVE NON MAIS OH
+                        pinData.m_name = GetPortalTag(hoverable);
+
+                        //TODO CHECK BEFORE REMOVE
                         if (closestPin != null)
                         {
                             Minimap.instance.RemovePin(closestPin);
                         }
 
-                        Minimap.instance.AddPin(pos, pinData.m_type, pinData.m_name, true, false, 0L);
+                        Minimap.instance.AddPin(pinData.m_pos, pinData.m_type, pinData.m_name, true, false, 0L);
 
                         //INFO
-                        QuickPing.Log.LogInfo($"Add Pin : Name:{pinData.m_name} x:{pos.x}, y:{pos.y}, Type:{pinData.m_type}");
+                        QuickPingPlugin.Log.LogInfo($"Add Pin : Name:{pinData.m_name} x:{pinData.m_pos.x}, y:{pinData.m_pos.y}, Type:{pinData.m_type}");
                     }
                 }
                 //OTHERS
                 else if (closestPin == null)
                 {
-                    Minimap.instance.AddPin(pos, pinData.m_type, pinData.m_name, true, false, 0L);
+                    Minimap.instance.AddPin(pinData.m_pos, pinData.m_type, pinData.m_name, true, false, 0L);
 
                     //INFO
-                    QuickPing.Log.LogInfo($"Add Pin : Name:{pinData.m_name} x:{pos.x}, y:{pos.y}, Type:{pinData.m_type}");
+                    QuickPingPlugin.Log.LogInfo($"Add Pin : Name:{pinData.m_name} x:{pinData.m_pos.x}, y:{pinData.m_pos.y}, Type:{pinData.m_type}");
                 }
-                if(idestructible != null)
+
+            }
+            else if (force && closestPin == null)
+            {
+                pinData.m_type = Settings.DefaultPinType.Value;
+                pinData.m_name ??= Localization.instance.Localize(strID);
+                pinData.m_pos = pos;
+                switch (pinData.m_name)
                 {
-                    switch (idestructible)
-                    {
-                        case Destructible d_:
-                            //d_.m_nview.SetPersistent(true);
-                            if (!d_.m_nview.m_functions.ContainsKey("RemovePin".GetStableHashCode()))
-                                d_.m_nview.Register<Vector3>("RemovePin", RPC_RemovePin);
-                            break;
-
-                        case WearNTear d_:
-                            //d_.m_nview.SetPersistent(true);
-                            if (!d_.m_nview.m_functions.ContainsKey("RemovePin".GetStableHashCode()))
-                                d_.m_nview.Register<Vector3>("RemovePin", RPC_RemovePin);
-                            break;
-
-                        case MineRock5 d_:
-                            //d_.m_nview.SetPersistent(true);
-                            if (!d_.m_nview.m_functions.ContainsKey("RemovePin".GetStableHashCode()))
-                                d_.m_nview.Register<Vector3>("RemovePin", RPC_RemovePin);
-                            break;
-                    }
+                    case null:
+                    case "":
+                        pinData.m_name = Settings.DefaultPingText;
+                        break;
                 }
+                Minimap.instance.AddPin(pinData.m_pos, pinData.m_type, pinData.m_name, true, false, 0L);
+
+                //INFO
+                QuickPingPlugin.Log.LogInfo($"Force Add Pin : Name:{pinData.m_name} x:{pos.x}, y:{pos.y}, Type:{pinData.m_type}");
+            }
+
+            if (idestructible != null)
+            {
+                FieldInfo fieldInfo = idestructible.GetType().GetField("m_nview", BindingFlags.Instance | BindingFlags.NonPublic);
+                ZNetView netView = fieldInfo.GetValue(idestructible) as ZNetView;
+                ZDOID uid = netView.GetZDO().m_uid;
+                if (!PinnedObjects.ContainsKey(uid))
+                    PinnedObjects.Add(uid, pinData);
             }
         }
 
@@ -185,21 +201,21 @@ namespace QuickPing.Patches
         }
 
         /// <summary>
-        /// 
+        /// Find pin at pos
         /// </summary>
         /// <param name="pos"></param>
         /// <param name="type"></param>
         /// <param name="name"></param>
         /// <returns></returns>
-        public static PinData FindPin(Vector3 pos, PinType type = PinType.None, string name = null)
+        public static Minimap.PinData FindPin(Vector3 pos, Minimap.PinType type = Minimap.PinType.None, string name = null)
         {
-            var pins = new List<(PinData pin, float dis)>();
+            var pins = new List<(Minimap.PinData pin, float dis)>();
 
             foreach (var pin in Minimap.instance.m_pins)
-                if (type == PinType.None || pin.m_type == type)
+                if (type == Minimap.PinType.None || pin.m_type == type)
                     pins.Add((pin, Utils.DistanceXZ(pos, pin.m_pos)));
 
-            PinData closest = null;
+            Minimap.PinData closest = null;
             float closestDis = float.MaxValue;
 
             foreach (var (pin, dis) in pins)
@@ -219,19 +235,86 @@ namespace QuickPing.Patches
         }
 
         /// <summary>
-        /// 
+        /// Remove pin pinData (need to be exact value)
         /// </summary>
-        /// <param name="pos"></param>
-        /// <param name="type"></param>
-        /// <param name="name"></param>
-        /// <returns></returns>
-        public static void RPC_RemovePin(long sender, Vector3 pos) => RemovePin(pos, PinType.None, null);
-        public static void RemovePin(Vector3 pos, PinType type = PinType.None, string name = null)
+        /// <param name="pinData"></param>
+        public static void RemovePin(Minimap.PinData pinData)
         {
-            if (FindPin(pos, type, name) is not PinData pin)
+            if (pinData == null) return;
+            Minimap.instance.RemovePin(pinData);
+        }
+
+        /// <summary>
+        /// Remove pin in pos, of type and name
+        /// </summary>
+        /// <param name="pos">Pin to remove position</param>
+        /// <param name="type">Pin to remove type</param>
+        /// <param name="name">Pin to remove name</param>
+        public static void RemovePin(Vector3 pos, Minimap.PinType type = Minimap.PinType.None, string name = null)
+        {
+            if (FindPin(pos, type, name) is not Minimap.PinData pin)
                 return;
 
             Minimap.instance.RemovePin(pin);
         }
+
+
+        /// <summary>
+        /// Read ZPackage into PinnedObjects list
+        /// </summary>
+        /// <param name="package">Loaded from file</param>
+        public static void ReadPinData(ZPackage package)
+        {
+            if (package == null) return;
+
+            while (package.GetPos() < package.Size())
+            {
+                ZDOID zdoid = package.ReadZDOID();
+                Minimap.PinData pinData = new()
+                {
+                    m_name = package.ReadString(),
+                    m_type = (Minimap.PinType)Enum.Parse(typeof(Minimap.PinType), package.ReadString()),
+                    m_pos = package.ReadVector3()
+                };
+                if (!PinnedObjects.ContainsKey(zdoid))
+                    PinnedObjects.Add(zdoid, pinData);
+
+            }
+
+        }
+
+        /// <summary>
+        /// Pack ZDOID, name, type and position in ZPackage and returns it.
+        /// </summary>
+        /// <returns>ZPackage containing ZDOID, name, type and position</returns>
+        public static ZPackage PackData()
+        {
+            ZPackage zPackage = new ZPackage();
+
+            foreach (var x in Minimap_Patch.PinnedObjects)
+            {
+                zPackage.Write(x.Key);
+                zPackage.Write(x.Value.m_name);
+                zPackage.Write(x.Value.m_type.ToString());
+                zPackage.Write(x.Value.m_pos);
+            }
+            return zPackage;
+        }
+
+        /// <summary>
+        /// Save all pinned into file (support SteamStorage cloud)
+        /// </summary>
+        /// <param name="world"></param>
+        /// <param name="cloudSaveFailed"></param>
+        public static void SavePinnedDataToWorld(World world, out bool cloudSaveFailed)
+        {
+            ZPackage zPackage = PackData();
+
+            cloudSaveFailed = Utilities.DataUtils.SaveData(world, zPackage);
+
+        }
+
     }
+
+
 }
