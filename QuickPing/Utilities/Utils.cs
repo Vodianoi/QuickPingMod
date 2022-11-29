bb40e8d348989d1a192abccf6edc5e6209e6460a
Vodianoi <axepoty@gmail.com>
2022-11-29 18:42:20 +0100

    Add Data utils

--- QuickPing/Utilities/Utils.cs
+++ QuickPing/Utilities/Utils.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 
 namespace QuickPing.Utilities
 {
@@ -26,6 +27,63 @@ namespace QuickPing.Utilities
             }
             else
                 return GetRecursiveParentWithComponent<T>(root.parent);
+        }
+
+
+    }
+    internal static class DataUtils
+    {
+
+        public static byte[] ReadAllBytes(this BinaryReader reader)
+        {
+            const int bufferSize = 4096;
+            using (var ms = new MemoryStream())
+            {
+                byte[] buffer = new byte[bufferSize];
+                int count;
+                while ((count = reader.Read(buffer, 0, buffer.Length)) != 0)
+                    ms.Write(buffer, 0, count);
+                return ms.ToArray();
+            }
+        }
+        public static bool SaveData(World world, ZPackage zPackage)
+        {
+            bool cloudSaveFailed;
+            if (world.m_fileSource != FileHelpers.FileSource.Cloud)
+            {
+                Directory.CreateDirectory(World.GetWorldSavePath(world.m_fileSource));
+            }
+
+            string metaPath = GetPinnedPath(world);
+            string text = metaPath + ".new";
+            string oldFile = metaPath + ".old";
+            byte[] array = zPackage.GetArray();
+            FileWriter fileWriter = new FileWriter(text, FileHelpers.FileHelperType.Binary, world.m_fileSource);
+            fileWriter.m_binary.Write(array.Length);
+            fileWriter.m_binary.Write(array);
+            fileWriter.Finish();
+
+
+            cloudSaveFailed = fileWriter.Status != FileWriter.WriterStatus.CloseSucceeded && world.m_fileSource == FileHelpers.FileSource.Cloud;
+            if (!cloudSaveFailed)
+            {
+                FileHelpers.ReplaceOldFile(metaPath, text, oldFile, world.m_fileSource);
+            }
+
+            return cloudSaveFailed;
+        }
+
+        public static string GetPinnedPath(World world)
+        {
+            FileHelpers.SplitFilePath(world.GetDBPath(), out string directory, out string fileName, out string fileExtension);
+            var worldSavePath = directory + fileName;
+            const string __MyExtension = ".mod.quickping.pinned";
+
+
+            //INFO
+            //QuickPingPlugin.Log.LogInfo("World .pinned save path: " + worldSavePath);
+
+            return worldSavePath + __MyExtension;
         }
     }
 }
