37679e2b2d7c9eadaee76cf0d0c964ada03a7e71
Vodianoi <axepoty@gmail.com>
2022-12-03 23:07:32 +0100

    Update Utils.cs
    Add extensions for ZPackage

--- QuickPing/Utilities/Utils.cs
+++ QuickPing/Utilities/Utils.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 namespace QuickPing.Utilities
@@ -85,5 +87,54 @@ namespace QuickPing.Utilities
 
             return worldSavePath + __MyExtension;
         }
+
+        public static void Write(this ZPackage zPackage, Minimap.PinData pinData)
+        {
+            zPackage.Write(pinData.m_name);
+            zPackage.Write(pinData.m_type.ToString());
+            zPackage.Write(pinData.m_pos);
+        }
+
+
+        /// <summary>
+        /// Read ZPackage into PinnedObjects list
+        /// </summary>
+        /// <param name="package">Loaded from file</param>
+        public static Dictionary<ZDOID, Minimap.PinData> ReadPinnedObjects(this ZPackage package)
+        {
+            Dictionary<ZDOID, Minimap.PinData> res = new Dictionary<ZDOID, Minimap.PinData>();
+
+            if (package == null) return res;
+
+            while (package.GetPos() < package.Size())
+            {
+                ZDOID zdoid = package.ReadZDOID();
+                Minimap.PinData pinData = package.ReadPinData();
+                if (!res.ContainsKey(zdoid))
+                    res.Add(zdoid, pinData);
+
+            }
+
+            return res;
+
+        }
+
+        private static Minimap.PinData ReadPinData(this ZPackage package)
+        {
+            return new()
+            {
+                m_name = package.ReadString(),
+                m_type = (Minimap.PinType)Enum.Parse(typeof(Minimap.PinType), package.ReadString()),
+                m_pos = package.ReadVector3()
+            };
+        }
+
+        public static bool Compare(this Minimap.PinData x, Minimap.PinData y)
+        {
+            return x.m_pos == y.m_pos
+                && x.m_name == y.m_name
+                && x.m_type == y.m_type;
+        }
+
     }
 }
