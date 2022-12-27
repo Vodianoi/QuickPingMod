f0ea308aec7f151dd2a52ca4b852bdfbfe4093de
Vodianoi <axepoty@gmail.com>
2022-12-28 00:31:31 +0100

    libs assemblyinfo

--- QuickPing/Properties/AssemblyInfo.cs
+++ QuickPing/Properties/AssemblyInfo.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -27,6 +29,6 @@
 //      Revision
 //
 //JObject
-[assembly: AssemblyVersion("1.5.3.0")]
-[assembly: AssemblyFileVersion("1.5.3.0")]
+//[assembly: AssemblyVersion("1.5.3.0")]
+//[assembly: AssemblyFileVersion("1.5.3.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
